3b30a1ea69c7f5bd8607b90d99175e40dd357921
Diego Vilhlava <diegorocha450@yahoo.com>
2024-04-15 13:50:49 -0300

    About section finished

--- src/sections/About.jsx
+++ src/sections/About.jsx
@@ -1,8 +1,59 @@
-
+import icon1 from "../assets/icon1.png"
+import icon2 from "../assets/icon2.png"
+import icon3 from "../assets/icon3.png"
+import icon4 from "../assets/icon4.png"
+import about1 from "../assets/about1.jpg"
+import { useDarkMode } from "../DarkModeContext"
+import Aos from "aos"
+import 'aos/dist/aos.css'
+import { useEffect } from "react"
 
 const About = () => {
+  useEffect(() => {
+    Aos.init({
+      offset: 200,
+      duration: 800,
+      easing: "ease-in-sine",
+      delay: 100
+    })
+  }, [])
+  const {darkMode,toggleDarkMode} = useDarkMode()
   return (
-    <div>About</div>
+    <>
+      <section id="about" className={`${darkMode ? 'dark bg-black' : 'light bg-white'} w-full lg:px-20 px-10 lg:py-20 py-10 h-fit grid lg:grid-cols-4 grid-cols-1 justify-center items-center gap-10`}>
+        <div data-aos="zoom-in" data-aos-delay="200" className="border-[4px]  border-orange-600 dark:border-orange-400 p-14 flex  flex-col justify-center items-center gap-6 rounded-lg hover:bg-orange-100 cursor-point">
+          <img src={icon1} alt="pizza icon" className="w-[80px] h-[80px] transform hover:scale-105
+          transition-transform duration-300 cursor-pointer" />
+          <h1 className="text-black text-2xl font-lg  text-center">Os Melhores Sabores</h1>
+        </div>
+        <div data-aos="zoom-in" data-aos-delay="200" className="border-[4px]  border-orange-600 dark:border-orange-400 p-14 flex  flex-col justify-center items-center gap-6 rounded-lg hover:bg-orange-100 cursor-point">
+          <img src={icon2} alt="pizza icon" className="w-[80px] h-[80px] transform hover:scale-105
+          transition-transform duration-300 cursor-pointer" />
+          <h1 className="text-black text-2xl font-lg  text-center">Ingedientes Organicos</h1>
+        </div>
+        <div data-aos="zoom-in" data-aos-delay="200" className="border-[4px]  border-orange-600 dark:border-orange-400 p-14 flex  flex-col justify-center items-center gap-6 rounded-lg hover:bg-orange-100 cursor-point">
+          <img src={icon3} alt="pizza icon" className="w-[80px] h-[80px] transform hover:scale-105
+          transition-transform duration-300 cursor-pointer" />
+          <h1 className="text-black text-2xl font-lg  text-center">Agilidade na Produção</h1>
+        </div>
+        <div data-aos="zoom-in" data-aos-delay="200" className="border-[4px]  border-orange-600 dark:border-orange-400 p-14 flex  flex-col justify-center items-center gap-6 rounded-lg hover:bg-orange-100 cursor-point">
+          <img src={icon4} alt="pizza icon" className="w-[80px] h-[80px] transform hover:scale-105
+          transition-transform duration-300 cursor-pointer" />
+          <h1 className="text-black text-2xl font-lg  text-center">Massas Caseiras</h1>
+        </div>
+      </section>
+      <section className={`${darkMode ? 'dark bg-black' : 'light bg-white'} w-full lg:px-20 px-10 lg:py-20 py-10 h-fit  grid lg:grid-cols-2 grid-cols-1 justify-center items-center gap-10`}>
+        <div>
+          <img src={about1} alt="about image" className="lg:w-[800px] lg:h-[600px] rounded-xl" />
+        </div>
+        <div className="flex flex-col justify-center items-center gap-8">
+            <h1 data-aos="zoom-in" className="text-2xl text-black dark:text-white font-semibold text-center">Bem vindo a</h1>
+            <h1 data-aos="zoom-in" className="text-6xl text-black dark:text-white font-bold text-center">La Piazza Pizzaria</h1>
+            <p data-aos="zoom-in" className="text-xl text-slate-800 dark:text-slate-100 text-center">Na nossa pizzaria, cada fatia é uma experiência única, preparada com ingredientes frescos e paixão, para satisfazer os mais exigentes paladares</p>
+            <button data-aos="flip-up" className="bg-orange-500 text-lg text-white px-8 py-4 rounded-full font-semibold hover:bg-white hover:text-black dark:hover:text-black">Peça já</button>
+        </div>
+      </section>
+    </>
   )
 }
 
--- src/sections/Hero.jsx
+++ src/sections/Hero.jsx
@@ -2,6 +2,7 @@ import banner4 from "../assets/banner4.jpg"
 import Aos from "aos"
 import 'aos/dist/aos.css'
 import { useEffect } from "react"
+
 const Hero = () => {
 
   useEffect(() => {
@@ -12,10 +13,12 @@ const Hero = () => {
       delay:100
     })
   },[])
+
+
   return (
-    <section id="hero" className="w-full lg:px-20 px-10 lg:py-20 py-10 h-[600px] bg-cover bg-center flex flex-col justify-center items-center gap-8" style={{backgroundImage:`url(${banner4})`}} >
-      <h1 data-aos="zoom-in" className="text-6xl text-white font-bold">Vida é uma combinação de Pizza & Magia</h1>
-      <p data-aos="zoom-in">Em nossa pizzaria, celebramos heróis cotidianos com saborosas pizzas que alimentam corações e inspiram histórias memoráveis</p>
+    <section id="hero" className="w-full lg:px-20 px-10 lg:py-20 py-10 h-[600px] bg-cover bg-center flex flex-col justify-center items-start gap-8" style={{backgroundImage:`linear-gradient(rgba(0, 0, 0, 0.7), rgba(0, 0, 0, 0.7)),url(${banner4})`}} >
+      <h1 data-aos="zoom-in" className="text-6xl text-white font-bold aos-init aos-animate">Vida é uma combinação de Pizza & Magia</h1>
+      <p data-aos="zoom-in" className="text-slate-50 drop-shadow-sm text-xl">Em nossa pizzaria, celebramos heróis cotidianos com saborosas pizzas que alimentam corações e inspiram histórias memoráveis</p>
       <button data-aos="flip-up" className="bg-orange-500 text-lg text-white px-8 py-4 rounded-full font-semibold hover:bg-white hover:text-black dark:hover:text-black">Peça já</button>
     </section>
   )
