f74c95c3f455df089abcffbb2bb612b42a4e2c7f
Diego Vilhlava <diegorocha450@yahoo.com>
2024-04-15 11:44:55 -0300

    Hero section started

---
+++ src/components/export.js
@@ -0,0 +1,46 @@
+import cheese from "../assets/cheese.jpg";
+import delicious from "../assets/delicious.jpg";
+import peproni from "../assets/peproni.jpg";
+import rustica from "../assets/rustica.jpg";
+import tomato from "../assets/tomato.jpg";
+import vege from "../assets/vege.jpg";
+
+export const pizza = [
+{
+    image: cheese,
+    title: "Pizza de Queijo",
+    para: "Experimente nossa Manhattan Classic Cheese Pizza com sua escolha de molho e massa.",
+    price: "R$ 1,48",
+  },
+  {
+    image: delicious,
+    title: "Pizza Deliciosa",
+    para: "Experimente nossa Manhattan Classic Cheese Pizza com sua escolha de molho e massa.",
+    price: "R$ 7,01",
+  },
+  {
+    image: peproni,
+    title: "Pizza de Pepperoni",
+    para: "Experimente nossa Manhattan Classic Cheese Pizza com sua escolha de molho e massa.",
+    price: "R$ 16,65",
+  },
+  {
+    image: rustica,
+    title: "Pizza Rústica",
+    para: "Experimente nossa Manhattan Classic Cheese Pizza com sua escolha de molho e massa.",
+    price: "R$ 10,21",
+  },
+  {
+    image: tomato,
+    title: "Pizza de Tomate",
+    para: "Experimente nossa Manhattan Classic Cheese Pizza com sua escolha de molho e massa.",
+    price: "R$ 24,64",
+  },
+  {
+    image: vege,
+    title: "Pizza Vegetariana",
+    para: "Experimente nossa Manhattan Classic Cheese Pizza com sua escolha de molho e massa.",
+    price: "R$ 10,93",
+  },
+
+];
--- src/sections/Hero.jsx
+++ src/sections/Hero.jsx
@@ -1,8 +1,23 @@
-
-
+import banner4 from "../assets/banner4.jpg"
+import Aos from "aos"
+import 'aos/dist/aos.css'
+import { useEffect } from "react"
 const Hero = () => {
+
+  useEffect(() => {
+    Aos.init({
+      offset:200,
+      duration:800,
+      easing:"ease-in-sine",
+      delay:100
+    })
+  },[])
   return (
-    <div>Hero</div>
+    <section id="hero" className="w-full lg:px-20 px-10 lg:py-20 py-10 h-[600px] bg-cover bg-center flex flex-col justify-center items-center gap-8" style={{backgroundImage:`url(${banner4})`}} >
+      <h1 data-aos="zoom-in" className="text-6xl text-white font-bold">Vida é uma combinação de Pizza & Magia</h1>
+      <p data-aos="zoom-in">Em nossa pizzaria, celebramos heróis cotidianos com saborosas pizzas que alimentam corações e inspiram histórias memoráveis</p>
+      <button data-aos="flip-up" className="bg-orange-500 text-lg text-white px-8 py-4 rounded-full font-semibold hover:bg-white hover:text-black dark:hover:text-black">Peça já</button>
+    </section>
   )
 }
 
